b2e164aacd94214f69c9453e2a8d33fe60113824
huangered <huangered@hotmail.com>
2020-11-20 15:30:29 +0800

    update sql

--- auth-server/src/main/resources/db/migration/V1__Initial.sql
+++ auth-server/src/main/resources/db/migration/V1__Initial.sql
@@ -1,4 +1,17 @@
-CREATE TABLE IF NOT EXISTS app_client_entity
+CREATE TABLE IF NOT EXISTS user_entity
+(
+    id serial,
+    account_non_expired boolean NOT NULL,
+    account_non_locked boolean NOT NULL,
+    credentials_non_expired boolean NOT NULL,
+    enabled boolean NOT NULL,
+    password text NOT NULL,
+    telephone text,
+    username text NOT NULL,
+    CONSTRAINT user_entity_pkey PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS app_client_entity2
 (
     id serial,
     user_id bigint,
@@ -10,7 +23,8 @@ CREATE TABLE IF NOT EXISTS app_client_entity
     access_token_validity_seconds int,
     refresh_token_validity_seconds int,
     auto_approve boolean,
-    CONSTRAINT app_client_entity_pkey PRIMARY KEY (id)
+    CONSTRAINT app_client_entity_pkey PRIMARY KEY (id),
+    CONSTRAINT app_client_entity_fkey FOREIGN KEY (user_id) REFERENCES user_entity (id)
 );
 
 CREATE TABLE IF NOT EXISTS app_granted_authority_entity
@@ -18,18 +32,7 @@ CREATE TABLE IF NOT EXISTS app_granted_authority_entity
     id serial,
     authority text,
     user_id bigint,
-    CONSTRAINT app_granted_authority_entity_pkey PRIMARY KEY (id)
+    CONSTRAINT app_granted_authority_entity_pkey PRIMARY KEY (id),
+    CONSTRAINT app_granted_authority_entity_fkey FOREIGN KEY (user_id) REFERENCES user_entity (id)
 );
 
-CREATE TABLE IF NOT EXISTS user_entity
-(
-    id serial,
-    account_non_expired boolean NOT NULL,
-    account_non_locked boolean NOT NULL,
-    credentials_non_expired boolean NOT NULL,
-    enabled boolean NOT NULL,
-    password text,
-    telephone text,
-    username text,
-    CONSTRAINT user_entity_pkey PRIMARY KEY (id)
-);
