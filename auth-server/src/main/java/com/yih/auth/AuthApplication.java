afdd84593f0f24858961086b33966ad46360feda
huangered <huangered@hotmail.com>
2020-11-20 16:33:13 +0800

    update test

--- auth-server/src/main/java/com/yih/auth/AuthApplication.java
+++ auth-server/src/main/java/com/yih/auth/AuthApplication.java
@@ -3,7 +3,6 @@ package com.yih.auth;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication(scanBasePackages = {"com.yih"})
 public class AuthApplication {
 
--- auth-server/src/test/java/com/yih/app/AuthApplicationTests.java
+++ auth-server/src/test/java/com/yih/app/AuthApplicationTests.java
@@ -1,15 +1,75 @@
 package com.yih.app;
 
+import com.yih.auth.AuthApplication;
+import com.yih.auth.ctl.AppUserCtl;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
 
-//@SpringBootTest()
-class AuthApplicationTests {
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+@SpringBootTest(classes = AuthApplication.class)
+@WebAppConfiguration
+public class AuthApplicationTests {
+
+    protected MockMvc mvc;
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    protected void setUp() {
+        mvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
-    void contextLoads() {
+    public void contextLoads() {
         System.out.println("hello world");
     }
 
+    @Test
+    public void registerUserTest() throws Exception {
+        AppUserCtl.RegisterUser user = new AppUserCtl.RegisterUser();
+        user.setUsername("ddd");
+        user.setPassword("ddd");
+        mvc.perform(post("/api/v1/users")
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .content(TestUtil.convertObjectToJsonBytes(user))
+        ).andExpect(status().isOk());
+    }
+}
+
+ class TestUtil {
+
+    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
+
+    public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        return mapper.writeValueAsBytes(object);
+    }
+
+    public static String createStringWithLength(int length) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int index = 0; index < length; index++) {
+            builder.append("a");
+        }
+
+        return builder.toString();
+    }
 }
