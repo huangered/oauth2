29785ca418fb164a50c70ba24896540c6c931995
huangered <huangered@hotmail.com>
2020-11-23 10:00:52 +0800

    add copyagentjar task

--- auth-server/src/test/java/com/yih/app/AuthApplicationTests.java
+++ auth-server/src/test/java/com/yih/app/AuthApplicationTests.java
@@ -53,23 +53,3 @@ public class AuthApplicationTests {
     }
 }
 
- class TestUtil {
-
-    public static final MediaType APPLICATION_JSON_UTF8 = new MediaType(MediaType.APPLICATION_JSON.getType(), MediaType.APPLICATION_JSON.getSubtype(), Charset.forName("utf8"));
-
-    public static byte[] convertObjectToJsonBytes(Object object) throws IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return mapper.writeValueAsBytes(object);
-    }
-
-    public static String createStringWithLength(int length) {
-        StringBuilder builder = new StringBuilder();
-
-        for (int index = 0; index < length; index++) {
-            builder.append("a");
-        }
-
-        return builder.toString();
-    }
-}
